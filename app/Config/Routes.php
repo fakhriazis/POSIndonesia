b10f1d030fe5e9aebf3861e36069eb3e71e246f8
Dkurnia17 <74595792+Dkurnia17@users.noreply.github.com>
2020-12-16 05:14:27 +0700

    Add files via upload

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,13 +1,12 @@
-<?php
-
-namespace Config;
+<?php namespace Config;
 
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+{
 	require SYSTEMPATH . 'Config/Routes.php';
 }
 
@@ -32,18 +31,20 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/form', 'Home::form');
+$routes->get('/data', 'Home::data');
 
-//routes user
-$routes->get('/user', 'User/Admin::index');
-$routes->get('/user/referensi', 'User/Admin::referensi');
-$routes->get('/user/troubleshooting', 'User/Admin::troubleshooting');
-$routes->get('/user/kelolaFaq', 'User/Admin::kelolaFaq');
-$routes->get('/user/catatan', 'User/Admin::catatan');
-$routes->get('/user/catatan/tambah', 'User/Admin::viewTambahCatatan');
-$routes->get('/user/catatan/tambah/aksi', 'User/Admin::TambahCatatan');
-$routes->get('/user/berita', 'User/Admin::berita');
-$routes->get('/user/form', 'User/Admin::form');
-$routes->get('/user/data', 'User/Admin::data');
+
+$routes->get('/admin', 'Admin/CSAdmin::viewAdmin');
+$routes->get('/admin/tambah', 'Admin/CSAdmin::viewTambahAdmin');
+
+$routes->get('/layanan', 'Admin/CSAdmin::viewLayanan');
+
+$routes->get('/document', 'Admin/CSAdmin::viewDocument');
+$routes->get('/document/detail', 'Admin/CSAdmin::viewDocumentDetail');
+
+$routes->get('/dashboard', 'Admin/CSAdmin::dashboard');
+
 
 
 /**
@@ -59,6 +60,7 @@ $routes->get('/user/data', 'User/Admin::data');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
+{
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
