5da7a1a0168042f1e0ba140eb5ba4ae423352385
Fakhri Bashiri <fakhriazis994@gmail.com>
2020-12-19 09:19:43 +0700

    routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,12 +1,13 @@
-<?php namespace Config;
+<?php
+
+namespace Config;
 
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
-{
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
 	require SYSTEMPATH . 'Config/Routes.php';
 }
 
@@ -45,7 +46,6 @@ $routes->get('/document/detail', 'Admin/CSAdmin::viewDocumentDetail');
 
 $routes->get('/dashboard', 'Admin/CSAdmin::dashboard');
 
-<<<<<<< HEAD
 //routes user
 $routes->get('/user', 'User/Admin::index');
 
@@ -70,8 +70,6 @@ $routes->get('/user/data', 'User/Admin::data');
 $routes->get('/user/troubleshooting', 'User/Admin::troubleshooting');
 $routes->get('/user/referensi', 'User/Admin::referensi');
 $routes->get('/user/report', 'User/Admin::report');
-=======
->>>>>>> b10f1d030fe5e9aebf3861e36069eb3e71e246f8
 
 
 /**
@@ -87,7 +85,6 @@ $routes->get('/user/report', 'User/Admin::report');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
